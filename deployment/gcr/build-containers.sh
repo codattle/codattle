f062648a8a911bf08f80ac407da2233009383528
Sławomir <jestemKoderem@gmail.com>
2019-07-07 11:52:50 +0200

    deploy on cloud

--- deployment/gcr/build-containers.sh
+++ deployment/gcr/build-containers.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-docker build -t gcr.io/$PROJECT/codattle-core:$TRAVIS_COMMIT -f docker/core/Dockerfile core
-docker build -t gcr.io/$PROJECT/codattle-frontend:$TRAVIS_COMMIT -f docker/frontend/Dockerfile frontend
+docker build -t gcr.io/$PROJECT/codattle-core:$TRAVIS_COMMIT -f - core < docker/core/Dockerfile
+docker build -t gcr.io/$PROJECT/codattle-frontend:$TRAVIS_COMMIT -f - frontend < docker/frontend/Dockerfile
+docker build -t gcr.io/$PROJECT/codattle-simulation:$TRAVIS_COMMIT -f - simulation < docker/simulation/Dockerfile
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > gcloud-service-key.json
 gcloud auth activate-service-account --key-file gcloud-service-key.json
@@ -15,12 +16,15 @@ gcloud --quiet container clusters get-credentials $CLUSTER
 
 gcloud docker -- push gcr.io/$PROJECT/codattle-core
 gcloud docker -- push gcr.io/$PROJECT/codattle-frontend
+gcloud docker -- push gcr.io/$PROJECT/codattle-simulation
 
 yes | gcloud beta container images add-tag gcr.io/$PROJECT/codattle-core:$TRAVIS_COMMIT gcr.io/$PROJECT/codattle-core:latest
 yes | gcloud beta container images add-tag gcr.io/$PROJECT/codattle-frontend:$TRAVIS_COMMIT gcr.io/$PROJECT/codattle-frontend:latest
+yes | gcloud beta container images add-tag gcr.io/$PROJECT/codattle-simulation:$TRAVIS_COMMIT gcr.io/$PROJECT/codattle-simulation:latest
 
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/codattle-core codattle-core=gcr.io/$PROJECT/codattle-core:$TRAVIS_COMMIT
-kubectl set image deployment/codattle-frontend codattle-frontend=gcr.io/$PROJECT/codattle-frontend:$TRAVIS_COMMIT
+kubectl -n codattle set image deployment/codattle-core codattle-core=gcr.io/$PROJECT/codattle-core:$TRAVIS_COMMIT
+kubectl -n codattle set image deployment/codattle-frontend codattle-frontend=gcr.io/$PROJECT/codattle-frontend:$TRAVIS_COMMIT
+kubectl -n codattle set image deployment/codattle-simulation codattle-simulation=gcr.io/$PROJECT/codattle-simulation:$TRAVIS_COMMIT
